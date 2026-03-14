2dc3aa33ca51c37de26b67de0a5bd0536ddf768b
Amaan Kazi <amaankazi1793@gmail.com>
2026-03-14 14:28:18 +0530

    feat(main-menu): quit button

--- app/src/main/java/com/amaan/tictactoe/MainActivity.java
+++ app/src/main/java/com/amaan/tictactoe/MainActivity.java
@@ -1,6 +1,7 @@
 package com.amaan.tictactoe;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button quit = findViewById(R.id.quit);
+
+        quit.setOnClickListener(v -> {
+            finishAffinity();
         });
     }
 }
